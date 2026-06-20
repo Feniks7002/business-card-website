49a74ca35cb522bc5d884d02843deade6f631900
Feniks7002 <07piotrr07@gmail.com>
2026-06-20 19:37:13 +0200

    Fonts implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,28 @@
 import "./globals.css";
 import Link from "next/link"
+import { Inter, Montserrat_Alternates } from "next/font/google";
 
-export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
+const interFont = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const montserratAlternates = Montserrat_Alternates({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-headers"
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${interFont.className} ${montserratAlternates.variable}`}>
+
         <header className="w-full bg-gray-300">
           <nav className="flex justify-between px-8 py-6">
             <div className="flex items-center gap-3">
@@ -16,16 +34,20 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
               <h1>AG WOJ</h1>
             </div>
             <ul className="flex items-center gap-3">
-              <li><Link href={"/"}>Home</Link></li>
-              <li><Link href={"/about"}>AboutMe</Link></li>
-              <li><Link href={"/site-projects"}>Projects</Link></li>
-              <li><Link href={"/museum"}>NightMuseum</Link></li>
-              <li><Link href={"/decybelomierz"}>Decybelomierz</Link></li>
+              <li className="text-sm uppercase"><Link href={"/"}>Home</Link></li>
+              <li className="text-sm uppercase"><Link href={"/about"}>AboutMe</Link></li>
+              <li className="text-sm uppercase"><Link href={"/site-projects"}>Projects</Link></li>
+              <li className="text-sm uppercase"><Link href={"/museum"}>NightMuseum</Link></li>
+              <li className="text-sm uppercase"><Link href={"/decybelomierz"}>Decybelomierz</Link></li>
             </ul>
           </nav>
         </header>
-        {children}
-        {/* <footer className="w-full bg-gray-300 flex items-center justify-center p-4">
+
+        <main>
+          {children}
+        </main>
+
+        {/* <footer className="w-full mh-50 bg-gray-300 flex items-center justify-center p-4">
           <p>&copy; 2023 AG WOJ. All rights reserved.</p>
         </footer> */}
       </body>
